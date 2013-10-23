e6765dcba55f41bd7264ccedf66a7c1c64990d11
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-22 20:04:21 -0500

    Final CRUD including User Authentication methods, sessions, and login/logout w/ mentor Josh

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,39 @@
 get '/' do
-  # Look in app/views/index.erb
+  @user = User.find_by_id(session[:user_id])
   erb :index
 end
+
+get '/login' do
+  erb :login
+end
+
+
+post '/login' do
+  @user = User.authenticate(params[:email], params[:password])
+  if @user
+    session[:user_id] = @user.id
+    redirect '/'
+  else
+    erb :login
+  end
+end
+
+get '/logout' do
+  session.clear
+  redirect '/'
+end
+
+get '/create_user' do
+  erb :create_user
+end
+
+post '/create_user' do
+  @user = User.new(email: params[:email], password: params[:password], password_confirmation: params[:password_confirmation])
+  if @user.save
+    session[:user_id] = @user.id
+    redirect '/'
+  else
+    erb :create_user
+  end
+end
+
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,19 @@
 class User < ActiveRecord::Base
-  # Remember to create a migration!
+  validates :email, presence: true
+  validates :password, presence: true
+  validates :password, confirmation: true, on: :create
+
+  def password
+    @password ||= BCrypt::Password.new(password_digest)
+  end
+
+  def password=(new_password)
+    @password = BCrypt::Password.create(new_password)
+    self.password_digest = @password
+  end
+
+  def self.authenticate(email, password)
+    user = User.find_by_email(email)
+    user if user && (user.password == password)
+  end
 end
